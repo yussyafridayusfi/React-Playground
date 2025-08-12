e6ee41e2a03f7f2b82c4100b4f4acd6113020f8b
yussyafridayusfi <yussyafrida0299@gmail.com>
2025-08-12 21:54:54 +0700

    add variable and type intro javascript

---
+++ fullstack/index.js
@@ -0,0 +1,16 @@
+let createBlog = (title, body) => {
+
+  if (!title) {
+    throw new Error('A title is required')
+  }
+
+
+  if (!body) {
+    throw new Error("A title is required");
+  }
+
+  return alert(`${title} - ${body}`);
+
+}
+
+createBlog("new","thiss")
